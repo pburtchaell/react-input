283381b7e852b859a5b49f7cc785511f425f36b1
Patrick Burtchaell <patrick@pburtchaell.com>
2017-04-17 10:24:06 -0500

    Use prop-types package

--- src/form.js
+++ src/form.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Submit from './submit';
 import Field from './field';
 
